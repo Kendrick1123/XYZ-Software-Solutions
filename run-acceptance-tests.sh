c1414905a0e33d1b7f4e6c058bfe15789743ac97
Kendrick1123 <99151252+Kendrick1123@users.noreply.github.com>
2023-12-26 00:59:20 +0800

    Create run-acceptance-tests.sh

---
+++ run-acceptance-tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Script for running automated acceptance tests
+# Example: Run Selenium tests
+mvn test -Dtest=AcceptanceTestSuite
+
+# Acceptance testing
+echo "Running Acceptance Tests..."

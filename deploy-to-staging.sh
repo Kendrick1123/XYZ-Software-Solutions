ace187a2e24f0f9a8aaffd1badf0391396d7c5ec
Kendrick1123 <99151252+Kendrick1123@users.noreply.github.com>
2023-12-26 00:56:06 +0800

    Create deploy-to-staging.sh

---
+++ deploy-to-staging.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Script for deploying to the staging environment using Maven
+# Example: Deploy to a Tomcat server
+mvn tomcat7:redeploy
+
+# Staging deployment
+echo "Deploying to Staging..."

03f65112ec671025d5d985e7329ff02881e4c8b1
Kendrick1123 <99151252+Kendrick1123@users.noreply.github.com>
2023-12-26 00:56:41 +0800

    Create deploy-to-production.sh

---
+++ deploy-to-production.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Script for deploying to the production environment using Maven
+# Example: Deploy to a Tomcat server
+mvn tomcat7:redeploy
+
+# Production deployment
+echo "Deploying to Production..."
